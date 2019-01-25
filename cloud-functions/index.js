4bfe0af3b0c3f13fe35eac37aa64bb88fb98428d
whynotcrybot <alexanderkatsero@gmail.com>
2019-01-25 10:48:55 +0200

    parse dates

--- cloud-functions/index.js
+++ cloud-functions/index.js
@@ -1,7 +1,16 @@
 exports.startPeriod = (event, callback) => {
-  const pubsubMessage = event.data;
+  const pubsubMessageRaw = event.data;
+  const pubsubMessage = Buffer.from(pubsubMessage.data, 'base64').toString();
+  const { type, date, duration } = JSON.parse(pubsubMessage);
 
-  console.log('INCOMING DATA', Buffer.from(pubsubMessage.data, 'base64').toString());
+  console.log('INCOMING DATA', pubsubMessage);
+
+  const startDate = new Date(date);
+  const endDate = new Date(startDate.getTime() + duration);
+
+  console.log('Date', date)
+  console.log('Start date', startDate.toISOString())
+  console.log('End date', endDate.toISOString())
 
   callback();
 };
